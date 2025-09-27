acb0fb4c0451abf389372f7bb633628b4f59ebf1
Eslam Abdelghany <eslam322_1@hotmail.com>
2025-09-27 04:42:42 +0300

    chore : update config with the llm configs

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,3 +1,4 @@
+from tkinter import N
 from typing import List
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -17,6 +18,23 @@ class Settings(BaseSettings):
     MONGO_DB_NAME: str
     MONGO_INITDB_ROOT_USERNAME: str  # Add this field
     MONGO_INITDB_ROOT_PASSWORD: str  # Add this field
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+    AZURE_OPENAI_ENDPOINT: str = None
+    AZURE_OPENAI_API_KEY: str = None
+    AZURE_OPENAI_API_VERSION: str = None
+    AZURE_OPENAI_DEPLOYMENT_GPT4O: str = None
+    AZURE_OPENAI_DEPLOYMENT_EMBEDDINGS: str = None
+    AZURE_OPENAI_DEPLOYMENT_GPT4O_MINI: str = None
+    CHHERE_API_KEY: str = None
+
+    GENERATION_MOELL_ID: str = None
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+    # ========== Default Params =========
+    INPUT_DEFAULT_MAX_CHARS: int = None
+    GENERATION_DEFAULT_MAX_TOKENS: int = None
+    GENERATION_DEFAULT_TEMPERATURE: float = None
 
     class Config:
         # Get the project root directory (parent of src)
